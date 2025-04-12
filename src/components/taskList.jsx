0b775f25ed5adbed1003bdcf3547f46d9b50f720
deepak007soni <61106957+deepak007soni@users.noreply.github.com>
2025-04-12 21:59:59 +0530

    no todo added msg added

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -3,9 +3,11 @@ import TaskItem from "./taskItem";
 const TaskList = ({ todos, onUpdate }) => {
   return (
     <div className="mt-2 h-[60vh] overflow-auto">
-      {todos.map((todo) => (
-        <TaskItem key={todo.id} todo={todo} onUpdate={onUpdate} />
-      ))}
+      {todos?.length
+        ? todos.map((todo) => (
+            <TaskItem key={todo.id} todo={todo} onUpdate={onUpdate} />
+          ))
+        : "No Todos Added yet"}
     </div>
   );
 };
